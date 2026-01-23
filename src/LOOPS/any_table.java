a21abef81fdbdcdef38ba374412e1b03b12a1c7a
Arsh Mansuri <arsh.amin.may.2006@gmail.com>
2026-01-23 21:50:32 +0530

    Add basic loop program

---
+++ src/LOOPS/any_table.java
@@ -0,0 +1,12 @@
+package LOOPS;
+import java.util.*;
+public class any_table {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter n:");
+        int n= sc.nextInt();
+        for (int i = 1; i <=10 ; i++) {
+            System.out.println(n+"x"+i+"="+(n*i));
+        }
+    }
+}
--- src/LOOPS/basic_loop.java
+++ src/LOOPS/basic_loop.java
@@ -2,7 +2,7 @@ package LOOPS;
 
 public class basic_loop {
     public static void main(String[] args) {
-        for (int i=0;i<5;i++) {
+        for (int i=1;i<5;i++) {
             System.out.println(i);
         }
     }
